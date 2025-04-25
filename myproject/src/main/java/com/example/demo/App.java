0c7238175974b96132b254bd2ef83498d3a8cd9e
VIJAY <hosapetivijay18@gmail.com>
2025-04-25 11:02:41 +0530

    Update App.java

--- myproject/src/main/java/com/example/demo/App.java
+++ myproject/src/main/java/com/example/demo/App.java
@@ -5,18 +5,13 @@ package com.example.demo;
  *
  */
 public class App {
-	public in add*(int a,int b){
-	return a+b;
-	}
-
-    	public static void main( String[] args ){
-    		App app =new App();
-    		int result =app.add(2,3);
-    
-    
-        System.out.println("2+3 =" + result );
-        System.out.println("Succesfully");
-        
-        
-    }
+public int add(int a, int b) {
+return a + b;
+}
+public static void main(String[] args) {
+App app = new App();
+int result = app.add(2, 3);
+System.out.println("2 + 3 = " + result);
+System.out.println("Application executed successfully!");
+}
 }
